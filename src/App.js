8de7bb0ab379ece0cc504791c2b4c560956fe906
Gustavo <kg.2k.pl@gmail.com>
2024-05-05 00:29:58 -0400

    using the IF conditional outside of the JSX

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 function profile() {
-  const user = {
-    name: 'Carl Johnson',
-    alias: 'Ciyei',
-    imageUrl: 'https://i.pinimg.com/236x/4e/21/66/4e2166ecc3c3c87f3b2e66380ec29745.jpg',
-  };
+  const currentYear = 2024;
+  let message;
+  // Cuando se usa el IF normal si o si se debe utilizar el condicional, fuera de la estructura JSX
+  if (currentYear === 2024) {
+    message = 'Es el año 2024';
+  } else {
+    message = 'Es otro año';
+  }
   return (
     <>
-    {/* aplicando concatenacion dentro de las llaves */}
-      <h1>{'Nombre: ' + user.name}</h1>
-      <h1>{'Apodo: ' + user.alias}</h1>
-      <img
-        src={user.imageUrl}
-        alt={'Photo of ' + user.name}
-      />
+      {message}
     </>
   )
 }
