04075547326afaf6e03765c091d471ad173bd59c
Gustavo <kg.2k.pl@gmail.com>
2024-05-05 00:07:32 -0400

    diferencia entre comillas dobles y llaves para los parámetros JSX

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 function sayHelloWordWithStyleCss() {
-  var text = "Hello World";
+  const user = {
+    imageUrl: "https://images7.memedroid.com/images/UPLOADED645/6201803e9abd9.jpeg"
+  };
   return (
     <h1>
-      <p className="red-text">{text}</p>
+      {/* Aqui usamos comillas para el parametro className del JSX -> p, cuando es texto */}
+      <p className="red-text">Hello Bro !!!</p> 
+      {/* Aqui usamos llaves para el parametro src del JSX -> img, cuando es una variable JS */}
+      <img src={user.imageUrl} alt=""></img>
     </h1>
   )
 }
