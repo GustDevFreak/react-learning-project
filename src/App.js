4b364083af0315d29f6465e9035e6f91b000d6c8
Gustavo <kg.2k.pl@gmail.com>
2024-05-05 23:12:53 -0400

    updating the screen using the hook { useState } on a state variable

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-export default function MyButton() {
+import { useState } from "react";
+
+// declarando una variable de estado dentro del componente
+function MyButton() {
+  
+  // [estado actual, funcion que actualizara el estado]
+  const [count, setCount] = useState(0);
+
+  // defino el controlado de eventos
   function handleClick() {
-    alert('Hello World');
-  };
+    setCount(count + 1)
+  }
 
   return (
     <button onClick={handleClick}>
-      Click me
+      Clicked {count} times
     </button>
   );
 }
+
+// Ahora ejecuto el componente
+export default MyButton
