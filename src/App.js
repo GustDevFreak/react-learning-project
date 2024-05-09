92bb352533b1563a48f6e82b800c4c974eea0ff0
Gustavo <kg.2k.pl@gmail.com>
2024-05-08 21:39:39 -0400

    using the same component, with the same state, using PROPS

--- src/App.js
+++ src/App.js
@@ -1,39 +1,41 @@
 import { useState } from "react";
 
-function MyApp() {
-  return (
-    <>
-    <h1>El mismo componente, pero con estados independiente</h1>
-    <p>Esto quiere decir que con react se puede usar el mismo componente, y cada uno obtendrá su propio estado.</p>
-    <div>
-      <h2>Button 1</h2>
-      <MyButton />
-    </div>
-    <div>
-      <h2>Button 2</h2>
-      <MyButton />
-    </div>
-    </>
-  );
-}
+// Componente padre
+function MyApp() { 
 
-// declarando una variable de estado dentro del componente
-function MyButton() {
-  
-  // [estado actual, funcion que actualizara el estado]
+  // Primero definimos el estado y el controlador de click en el componente padre
   const [count, setCount] = useState(0);
 
-  // defino el controlado de eventos
   function handleClick() {
     setCount(count + 1)
   }
 
   return (
-    <button onClick={handleClick}>
+    <>
+    <h1>El mismo componente, pero con estados dependientes, usando PROPS</h1>
+    <p>Esto quiere decir que los mismos componentes ahora tendran el mismo estado.</p>
+    <p>Como funciona?</p>
+    <p>Desde el componente padre se pasa pasa el state y el handle como propiedad (PROPS) al componente hijo</p>
+    <div>
+      <h2>Button 1</h2>
+      <MyButton count={count} onClick={handleClick} />
+    </div>
+    <div>
+      <h2>Button 2</h2>
+      <MyButton count={count} onClick={handleClick} />
+    </div>
+    </>
+  );
+}
+
+// Componente hijo
+function MyButton({count, onClick}) {
+
+  return (
+    <button onClick={onClick}>
       Clicked {count} times
     </button>
   );
 }
 
-// Ahora ejecuto el componente
 export default MyApp
