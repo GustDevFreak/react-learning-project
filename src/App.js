b7ec1cba3ec8073b5f56959e26df4c3784280fd6
Gustavo <kg.2k.pl@gmail.com>
2024-07-03 09:53:01 -0400

    applying useEffect in click counter and traffic light

--- src/App.js
+++ src/App.js
@@ -1,128 +1,23 @@
-import { useState } from "react";
+import { useState, useEffect } from 'react';
 
-// Se hace que este componente este controlado por los accesorios que recibe (props).
-function Board({ xIsNext, squares, onPlay }) {
+export default function Welcome(props) {
+  const [counter, setCounter] = useState(0);
+  const [semaforo, setSemaforo] = useState(false);
 
-  function handleClick(i) {
-    if (squares[i] || calculateWinner(squares)) { // validamos si un cuadrado tiene valor, si es asi, no dejamos que siga el proceso de actualizar el estado.
-      return;                                     // Llamamos a la función calculateWinner para comprobar si un jugador ha ganado
-    }
-    const nextSquares = squares.slice();
-
-    if (xIsNext) {
-      nextSquares[i] = "X";
-    } else {
-      nextSquares[i] = "O";
-    }
-    onPlay(nextSquares);
+  useEffect(() => {
+  }, [semaforo])
+  
+  const contar = () => {
+    setCounter(counter + 1);
+    setSemaforo(!semaforo);
   }
 
-  const winner = calculateWinner(squares);
-  let status;
-  if (winner) {
-    status = "Winner: " + winner;
-  } else {
-    status = "Next player: " + (xIsNext ? "X" : "O");
-  }
-
-  // Ahora se pasa las PROP a cada uno del componente Square que se esta renderizando
   return (
-    <>
-      <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
-    </>
-  );
-}
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
-
-// Se prepara el componente Square para recibir el PROP
-function Square({ value, onSquareClick }) {
-  return <button className="square" onClick={onSquareClick}>{value}</button>;
-}
-
-// Agregando componente de nivel superior para mostrar lista de movimientos anteriores, y desde aqui se llama al Board
-export default function Game() {
-  const [history, setHistory] = useState([Array(9).fill(null)]);
-  const [currentMove, setCurrentMove] = useState(0);
-  // (4) Fue lo ultimo que se cambio para simplificar codigo
-  const xIsNext = currentMove % 2 === 0;
-
-  // (3) Para renderizar el movimiento seleccionado actualmente, en lugar de siempre el movimiento final
-  const currentSquares = history[currentMove];
-
-  // Funcion que llamara el componente para actualizar el juego
-  function handlePlay(nextSquares) {
-    // (2) Crea un nuevo arreglo que combina la rebanada del historial hasta el 
-    // movimiento actual con el nuevo estado del tablero (nextSquares).
-    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
-    setHistory(nextHistory);
-    setCurrentMove(nextHistory.length - 1);
-    console.log(history);
-  }
-
-  function jumpTo(nextMove) {
-    setCurrentMove(nextMove);
-  }
-
-  const moves = history.map((squares, move) => {
-    let description;
-    if (move > 0) {
-      description = 'Go to move #' + move;
-    } else {
-      description = 'Go to game start';
-    }
-    return (
-      // (1) Los movimientos nunca se reordenarán, eliminarán ni se insertarán en el medio,
-      // por lo que es seguro usar el índice de movimientos como clave.
-      <li key={move}>
-        <button onClick={() => jumpTo(move)}>{description}</button>
-      </li>
-    );
-  });
-
-  return(
-    <div className="game">
-      <div className="game-board">
-        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
-      </div>
-      <div className="game-info">
-        <ol>
-          {moves}
-        </ol>
-      </div>
+    <div>
+      <h1>Contador</h1>
+      <h4>El numero del contador es: {counter}</h4>
+      <p>El semaforo esta en color {semaforo ? "red" : "green"}</p>
+      <button type='submit' onClick={contar}>Sumar contador</button>
     </div>
   );
 }
