aeac2fa645bc226858e63c0a7623352427ff9142
Gustavo <kg.2k.pl@gmail.com>
2024-07-03 10:36:33 -0400

    installing and applying react router dom

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Contact from "./pages/contact";
 
-export default function Welcome(props) {
-  const [counter, setCounter] = useState(0);
-  const [semaforo, setSemaforo] = useState(false);
-
-  useEffect(() => {
-  }, [semaforo])
-  
-  const contar = () => {
-    setCounter(counter + 1);
-    setSemaforo(!semaforo);
-  }
-
+function App() {
   return (
-    <div>
-      <h1>Contador</h1>
-      <h4>El numero del contador es: {counter}</h4>
-      <p>El semaforo esta en color {semaforo ? "red" : "green"}</p>
-      <button type='submit' onClick={contar}>Sumar contador</button>
-    </div>
-  );
+    // Encapsula nuestro componentes y renderiza las rutas
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </div>
+      <Routes>
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  )
 }
+
+export default (App);
