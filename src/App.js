a1ba50caf89427a78ef0dfdd28b1256ac01d35a8
Gustavo <kg.2k.pl@gmail.com>
2024-08-09 18:18:15 -0400

    react router dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import { ListTask } from './components/ListTask'
+import { Navigate, Route, Routes } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
+import { HomeScreen } from "./routes/HomeScreen";
+import { AboutScreen } from "./routes/AboutScreen";
+import { ContactScreen } from "./routes/ContactScreen";
 
 export default function App() {
   return (
-    <ListTask />
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomeScreen />}></Route>
+        <Route path="/about" element={<AboutScreen />}></Route>
+        <Route path="/contact" element={<ContactScreen />}></Route>
+        <Route path="/*" element={<Navigate to="/" />}></Route>
+      </Routes>
+    </>
   )
 }
